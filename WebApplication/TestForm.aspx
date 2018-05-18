863080e7dd2eec2838b3f891de99d2bfffbe9a23
NeilDyn <neil@dynetek.co.za>
2018-05-18 09:08:12 +0200

    Synchronization of Project to remote server.

---
+++ WebApplication/TestForm.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestForm.aspx.cs" Inherits="WebApplication.TestForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Enter Search No."></asp:Label>
+        <asp:TextBox ID="txtSearchNo" runat="server" style="margin-left: 13px" Width="170px" ForeColor="#CCCCCC"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Search" style="margin-left: 10px" Width="126px" OnClick="Button1_Click" />
+
+
+
+    </div>
+        <asp:GridView ID="gdvOrder" runat="server" Height="262px" Width="1287px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplication/TestForm.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication.Codeunit;
+using WebApplication.Class_Objects;
+
+namespace WebApplication
+{
+    public partial class TestForm : System.Web.UI.Page
+    {
+        private WebService webService;
+        private SearchResults searchResults;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string searchValue = txtSearchNo.Text;
+
+            if (searchValue != null && !string.IsNullOrWhiteSpace(searchValue))
+            {
+                webService = new WebService();
+
+                try
+                {
+                    searchResults = new SearchResults();
+                    searchResults = webService.FindOrder(searchValue);
+
+                    if (searchResults != null)
+                    {
+                        gdvOrder.DataSource = searchResults;
+                        gdvOrder.DataBind();
+                    }
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "XML port returned null value" + "');", true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + ex.Message + "');", true);
+                }                        
+            }
+        }
+    }
+}
